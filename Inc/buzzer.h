12de95d1e20592cf0645cd7ee2c980bdc512ce45
William Chang <wilicw1346@gmail.com>
2023-02-26 20:24:29 +0800

    feat(buzzer): new buzz lib

---
+++ Inc/buzzer.h
@@ -0,0 +1,28 @@
+//
+// Created by William Chang on 2023/2/22.
+//
+
+#ifndef AVIONICS_SLAVE_MCU_BUZZER_H
+#define AVIONICS_SLAVE_MCU_BUZZER_H
+
+#include <stdint.h>
+#include "stm32g0xx_hal.h"
+
+typedef struct {
+  uint8_t enabled;
+  uint32_t inter_counter;
+  uint32_t inter_div;
+  uint32_t div;
+  GPIO_TypeDef *port;
+  uint16_t pin;
+} buzz_t;
+
+void buzzer_tone(buzz_t *, uint16_t);
+
+void buzzer_enable(buzz_t *);
+
+void buzzer_disable(buzz_t *);
+
+void buzzer_handler(buzz_t *);
+
+#endif //AVIONICS_SLAVE_MCU_BUZZER_H
---
+++ Src/buzzer.c
@@ -0,0 +1,29 @@
+//
+// Created by William Chang on 2023/2/22.
+//
+
+#include "buzzer.h"
+
+void buzzer_tone(buzz_t *buzz, uint16_t tone) {
+  buzz->div = buzz->inter_div / tone;
+}
+
+void buzzer_enable(buzz_t *buzz) {
+  buzz->enabled = 1;
+}
+
+void buzzer_disable(buzz_t *buzz) {
+  buzz->enabled = 0;
+}
+
+void buzzer_handler(buzz_t *buzz) {
+  if (buzz->enabled) {
+    if (buzz->inter_counter % buzz->div == 0) {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
+    }
+    buzz->inter_counter = (buzz->inter_counter + 1) % buzz->div;
+  } else {
+    buzz->inter_counter = 0;
+    HAL_GPIO_WritePin(buzz->port, buzz->pin, 0);
+  }
+}
