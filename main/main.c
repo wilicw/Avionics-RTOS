071ed680f21d38f3aa4e315e5c36533fbd72d766
William Chang <wilicw1346@gmail.com>
2023-04-07 10:35:28 +0800

    fix: use esp_log_set_vprintf to redirect log

--- main/main.c
+++ main/main.c
@@ -12,6 +12,13 @@
 #include "task/sensors.h"
 #include "task/wdt.h"
 
+static FILE *f = NULL;
+
+static int log_vprintf(const char *fmt, va_list arguments) {
+  if (f == NULL) printf(fmt, arguments);
+  return vfprintf(f, fmt, arguments);
+}
+
 void app_main() {
   gpio_init();
   i2c_init();
@@ -20,17 +27,15 @@ void app_main() {
   sd_init();
   storage_init(NULL);
 
-  /* Logger redirection */
-  printf("Redirect log to file\n");
-  stdout = storage_fetch();
-  _GLOBAL_REENT->_stdout = storage_fetch();
+  f = storage_fetch();
+  esp_log_set_vprintf(log_vprintf);
 
-  vTaskDelay(pdMS_TO_TICKS(100));
+  vTaskDelay(pdMS_TO_TICKS(1000));
 
   xTaskCreatePinnedToCore(sensors_task, "sensors_task", 4096, NULL, 4, NULL, 0);
   xTaskCreatePinnedToCore(logger_task, "logger_task", 4096, NULL, 3, NULL, 0);
   xTaskCreatePinnedToCore(commu_task, "commu_task", 4096, NULL, 2, NULL, 0);
-  xTaskCreatePinnedToCore(wdt_task, "wdt_task", 2048, NULL, 1, NULL, 1);
+  // xTaskCreatePinnedToCore(wdt_task, "wdt_task", 2048, NULL, 1, NULL, 1);
 
   printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
 }
